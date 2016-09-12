ca8a3be87a37412590e4960cf9ca5546a06932a1
alavkx <alavoie3@gmail.com>
2016-09-12 13:22:30 -0400

    update main functionality
    working version of recursive search and compress. moves originals to new folder names

--- compressV3.js
+++ compressV3.js
@@ -3,49 +3,46 @@
 // https://nodejs.org/docs/latest/api/process.html
 'use strict';
 
-// Import promise library
-const Promise = require('bluebird');
-
-// Import the `imagemagick` library to compress our images
-// more speficially the convert function
 const convert = require('imagemagick').convert;
-
-// Import node's `file system` library to interact with files
 const fs = require('fs-extra');
-
-// Current working directory
 const cwd = require('process').cwd;
-
-// Import node's `child_process` library to create
-// a windows cmd shell that will execute our commands
-const exec = require('child_process').exec;
-
-// Import library for extended file directory functions
 const dir = require('node-dir');
-Promise.promisifyAll(dir);
-
-// Import data structure utils
 const R = require('ramda');
 
 // Removes all images from the list that are not compressed
-// noted by signature on filename `-C`
-const uncompressed = R.reject(item => item.includes('-C'));
+// noted by signature on filename `-C`,
+// or being located in folder `Uncompressed-Pictures`
+const uncompressed = R.reject(item => item.includes('-C') 
+			    || item.includes('Uncompressed-Pictures'));
 // Removes all images from the list that is not a jpg picture
 const isPicture = R.filter(item => item.toLowerCase().includes('jpg'));
 const sanitize = R.compose(uncompressed, isPicture);
 
+// Read files recursively and create an array of `filepath/filename`
 dir.files(cwd(), (err, files) => {
 	if (err) throw err;
 	const pics = sanitize(files);
 
-	R.forEach(
-		convert([files[i], '-resize', '1280x720', [files[i].replace(/(\.[\w\d_-]+)$/i, '-C$1')] ],
-		err => if (err) throw err;
-		)
-});
+	R.forEach((name) => {
+		// What we're here for
+		convert([name, '-resize', '1280x720', name.replace(/(\.[\w\d_-]+)$/i, '-C$1')],
+		err => {
+			if (err) throw err;
+			// Inserting folder in to filepath
+			let n = name.lastIndexOf('\\');
+			let uncompressed_filepath = name.substring(0,n) +'\\Uncompressed-Pictures';
 
-// - sanitize incoming list to only include
-//   uncompressed images
-// - break list in to two lists,
-//   compressed and uncompressed
-// - compress images in unc
+			// Create folder to store original pictures
+			fs.mkdirs(uncompressed_filepath, err => {
+				if (err) throw err;
+				let uncompressed_filename = uncompressed_filepath + '\\' + name.substring(n);
+
+				// Move uncompressed file to new folder
+				fs.move(name, uncompressed_filename, err => {
+					if (err) throw err;
+					console.log(name.substring(n) +  'moved to ' + 'Uncompressed-Pictures');
+	        	});
+	        });
+	    });
+	}, pics);
+});
